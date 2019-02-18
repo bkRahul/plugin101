ace189a1f27162569448c65247e5c93e3aa9c5e2
bkRahul <rahulsatya1068@gmail.com>
2019-02-18 23:55:14 +0530

    added a custom function store and instatiate the class

---
+++ inc/Init.php
@@ -0,0 +1,48 @@
+<?php
+/*
+
+ * @package plugin101
+ * @version 1.0.0
+
+*/
+
+namespace Inc;
+
+final class Init {
+	/**
+	* Store all the classes inside array
+	* @return array containing list of all classes
+	*/
+
+	public static function get_services() {
+		return [
+			Pages\Admin::class
+		];
+	}
+
+
+	/*
+	* Loop through the classes, initialize them 
+	* and call register method if exists
+	*/
+
+public static function register_services() {
+	foreach ( self::get_services() as $class ) {
+		$service = self::instantiate($class);
+		if( method_exists($service, 'register') ) {
+			$service->register();
+		}
+	}
+}
+
+	/**
+	* initialize the class
+	* @param class $class from services array 
+	* @return class instance new instance of the class
+	*/
+
+private static function instantiate( $class ) {
+	$service = new $class();
+	return $service;
+}
+}
